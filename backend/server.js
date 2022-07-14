41a34251deaddd42f3ff83e73dc65e85d5edb87e
Dave Malvin <dm.enterprienur@gmail.com>
2022-07-14 12:02:29 +0700

    backend(#2)
    + setting up cors

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const leaderboardRoutes = require('./routes/leaderboard')
+const cors = require('cors');
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors({ origin: true }));
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
